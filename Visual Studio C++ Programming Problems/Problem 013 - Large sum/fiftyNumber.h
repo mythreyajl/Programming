005cff6b13072cbd8bef37d39e63399280a18a2f
mjayendr <mjayendr@qti.qualcomm.com>
2014-08-08 13:48:40 -0700

    actually working commit

---
+++ Visual Studio C++ Programming Problems/Problem 013 - Large sum/fiftyNumber.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <algorithm> 
+#include <inttypes.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class fiftyNumber
+{
+private:
+	vector<int> fiftyDigitNumber;
+	vector<int> carry;
+public: 
+	fiftyNumber();
+	fiftyNumber(int);
+	fiftyNumber(int,int);
+	~fiftyNumber();
+	bool setDigit(int, int);
+	int getDigit(int);
+	fiftyNumber addNumber(fiftyNumber);
+};
+
+
