8a9f99b4868dc017e48c7b333e5494d56c8b76ef
Mythreya Jayendra Lakshman <mjayendr@qti.qualcomm.com>
2015-02-05 15:08:18 -0800

    Finishing touches to JavaScript project

--- MyWebsites/3. JavaScript/project/script.js
+++ MyWebsites/3. JavaScript/project/script.js
@@ -1,23 +1,28 @@
 var colorArray = ["red","purple","green","yellow","black","blue"];
-var origTopSq = document.getElementById("square").style.marginTop;
-var origLeftSq = document.getElementById("square").style.marginLeft;
+var origTopSq = document.getElementById("shape").style.marginTop;
+var origLeftSq = document.getElementById("shape").style.marginLeft;
 var prevTime = Date.now();
+var radius="100px";
 
-document.getElementById("square").onclick= function() {	
+document.getElementById("shape").onclick= function() {	
 	var currentTime = Date.now()-prevTime;
-	var color = document.getElementById("square").style.backgroundColor;
-	document.getElementById("square").style.backgroundColor="white";
+	var color = document.getElementById("shape").style.backgroundColor;
+	document.getElementById("shape").style.backgroundColor="white";
 	document.getElementById("text").innerHTML = "Reaction Time: "+currentTime+" ms";
 	setTimeout(function(){ resetSquare(); },1000);
 }
 
-function resetSquare() 	{
-	document.getElementById("square").style.backgroundColor=colorArray[randChoice()];
+function resetSquare() 	{	
+	document.getElementById("text").innerHTML = "";
+	document.getElementById("shape").style.borderRadius = "0px";
+	document.getElementById("shape").style.backgroundColor=colorArray[randChoice()];
 	var posTopSq = origTopSq+Math.floor(Math.random()*700);
 	var posLeftSq = origLeftSq+Math.floor(Math.random()*1600);
-	document.getElementById("square").style.marginTop=String(posTopSq)+"px";
-	document.getElementById("square").style.marginLeft=String(posLeftSq)+"px";
-	document.getElementById("text").style.marginLeft=String(posLeftSq)+"px";
+	document.getElementById("shape").style.marginTop=String(posTopSq)+"px";
+	document.getElementById("shape").style.marginLeft=String(posLeftSq)+"px";
+	if (Math.random()<Number(0.33)) {
+		document.getElementById("shape").style.borderRadius = radius;
+	}
 	prevTime = Date.now();
 }
 
