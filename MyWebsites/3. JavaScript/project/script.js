a48904dd61ffdcbd69ad7d99bde627a919bbcd41
Mythreya Jayendra Lakshman <mjayendr@qti.qualcomm.com>
2015-02-05 14:48:28 -0800

    Finishing JavaScript Project

--- MyWebsites/3. JavaScript/project/script.js
+++ MyWebsites/3. JavaScript/project/script.js
@@ -1,25 +1,24 @@
 var colorArray = ["red","purple","green","yellow","black","blue"];
 var origTopSq = document.getElementById("square").style.marginTop;
 var origLeftSq = document.getElementById("square").style.marginLeft;
-var origTopTx = document.getElementById("text").style.marginTop;
-var origLeftTx = document.getElementById("text").style.marginLeft;
+var prevTime = Date.now();
 
 document.getElementById("square").onclick= function() {	
+	var currentTime = Date.now()-prevTime;
 	var color = document.getElementById("square").style.backgroundColor;
 	document.getElementById("square").style.backgroundColor="white";
-	setTimeout(resetSquare(),3000);
+	document.getElementById("text").innerHTML = "Reaction Time: "+currentTime+" ms";
+	setTimeout(function(){ resetSquare(); },1000);
 }
 
 function resetSquare() 	{
 	document.getElementById("square").style.backgroundColor=colorArray[randChoice()];
-	var posTopSq = origTopSq+Math.floor(Math.random()*800);
+	var posTopSq = origTopSq+Math.floor(Math.random()*700);
 	var posLeftSq = origLeftSq+Math.floor(Math.random()*1600);
-	var posTopTx = origTopSq+Math.floor(Math.random()*800);
-	var posLeftTx = origLeftSq+Math.floor(Math.random()*1600);
 	document.getElementById("square").style.marginTop=String(posTopSq)+"px";
 	document.getElementById("square").style.marginLeft=String(posLeftSq)+"px";
-	document.getElementById("text").style.marginTop=String(posTopTx)+"px";
-	document.getElementById("text").style.marginLeft=String(posLeftTx)+"px";
+	document.getElementById("text").style.marginLeft=String(posLeftSq)+"px";
+	prevTime = Date.now();
 }
 
 function randChoice() {
