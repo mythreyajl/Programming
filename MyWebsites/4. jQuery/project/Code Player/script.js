2211adfda3caa6d0aee15f8e030cfc08b1ec258a
mjayendr <mjayendr@qti.qualcomm.com>
2015-02-09 08:33:59 -0800

    [jQuery]     Adding codes and project

---
+++ MyWebsites/4. jQuery/project/Code Player/script.js
@@ -0,0 +1,34 @@
+$("#validation-form").submit( function(event) {
+
+	var errorMessage = "";
+	
+	event.preventDefault();
+	
+	// Validation of each item
+	function isValidEmailAddress(emailAddress) {
+		var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+		return pattern.test(emailAddress);
+	};
+	
+	if (!isValidEmailAddress($("#email").val())) {
+		errorMessage = errorMessage + "Invalid email address. Please enter in valid format. <br/ >";
+	}
+	
+	if(!$.isNumeric($("#phone").val())) {
+		errorMessage = errorMessage + "Enter a valid phone number. <br/ >";
+	}
+	
+	
+	if( $("#pwd1").val() != $("#pwd2").val() ) {
+		errorMessage = errorMessage + "Enter a matching passwords. <br />";
+	}
+	
+	
+	if (errorMessage=="") {
+		alert("Success!");
+	} 
+	else {
+		errorMessage = errorMessage + "Please correct above errors before submitting!";
+		$("#error").html(errorMessage);
+	}
+});
